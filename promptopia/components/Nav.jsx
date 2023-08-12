14569a480b2b3df0ebf48699c18209a51ab34663
chaudhuree <chaudhuree00@gmail.com>
2023-08-12 09:54:12 +0600

    authenticatiion setup done with mongodb database and also checked functionality

--- promptopia/components/Nav.jsx
+++ promptopia/components/Nav.jsx
@@ -6,7 +6,7 @@ import {useEffect, useState} from "react";
 import {signIn, signOut, useSession, getProviders} from "next-auth/react";
 
 const Nav = () => {
-    const isAuthenticated = true;
+    const {data: session} = useSession()
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
@@ -33,7 +33,7 @@ const Nav = () => {
 
             {/* Desktop Navigation */}
             <div className='sm:flex hidden'>
-                {isAuthenticated ? (
+                {session?.user ? (
                     <div className='flex gap-3 md:gap-5'>
                         <Link href='/create-prompt' className='black_btn'>
                             Create Post
@@ -45,7 +45,7 @@ const Nav = () => {
 
                         <Link href='/profile'>
                             <Image
-                                src="/assets/images/logo.svg"
+                                src={session?.user?.image}
                                 width={37}
                                 height={37}
                                 className='rounded-full'
@@ -74,10 +74,10 @@ const Nav = () => {
 
             {/* Mobile Navigation */}
             <div className='sm:hidden flex relative'>
-                {isAuthenticated ? (
+                {session?.user ? (
                     <div className='flex'>
                         <Image
-                            src="/assets/images/logo.svg"
+                            src={session?.user?.image}
                             width={37}
                             height={37}
                             className='rounded-full'
