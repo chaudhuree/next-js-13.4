20390f5584e7da9e5426452d33b37ab79acb86f6
chaudhuree <chaudhuree00@gmail.com>
2023-08-12 07:42:30 +0600

    navigation structure ready. going to implement the functionality for next-auth

--- promptopia/app/layout.jsx
+++ promptopia/app/layout.jsx
@@ -1,21 +1,28 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts"
-}
+    title      : "Promptopia",
+    description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
-    return (
-        <html lang="en">
-        <body className="main">
-        <div class="gradient"/>
-        <main className="app">
+const RootLayout = ({children}) => (
+    <html lang='en'>
+    <body>
+    <Provider>
+        <div className='main'>
+            <div className='gradient'/>
+        </div>
+
+        <main className='app'>
+            <Nav/>
             {children}
         </main>
-        </body>
-        </html>
-    );
-};
+    </Provider>
+    </body>
+    </html>
+);
 
 export default RootLayout;
--- promptopia/app/page.jsx
+++ promptopia/app/page.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Feed from "@components/Feed";
+
 
 const Home = () => {
     return (
@@ -13,7 +15,7 @@ const Home = () => {
                 discover, create and share creative prompts
             </p>
 
-            {/*<Feed />*/}
+            <Feed/>
         </section>
     )
 }
